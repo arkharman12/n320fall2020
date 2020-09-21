483755d2ba11f9f4a95dfda8e2e356afed83f4a9
Harman Ark <ark.harman@gmail.com>
2020-09-20 20:14:48 -0400

    final changes to lab 3

--- labs/lab3/script.js
+++ labs/lab3/script.js
@@ -1,1 +1,56 @@
-// code goes here
+class VendingMachine {
+    constructor() {
+        this.snickers = 9
+        this.kitkat = 15
+        this.lays = 5
+    }
+
+    render() {
+        return `
+            <div>Snickers: ${this.snickers}</div>
+            <div>KitKat: ${this.kitkat}</div>
+            <div>Lay's: ${this.lays}</div>
+        `
+    }
+
+    buySnickers() {
+        if(this.snickers > 0) {
+            this.snickers--
+        } else {
+            this.render()
+            alert("Snickers are out of stock :(")
+        }
+    }
+    
+    buyKitkat() {
+        if(this.kitkat > 0) {
+            this.kitkat--
+        } else {
+            this.render()
+            alert("KitKats are out of stock :(")
+        }
+    }
+    
+    buyLays() {
+        if(this.lays > 0) {
+            this.lays--
+        } else {
+            this.render()
+            alert("Lay's are out of stock :(")
+        }
+    }
+}
+
+let aVendingMachone = new VendingMachine()
+machineDiv.innerHTML = aVendingMachone.render()
+
+function runMachine(clickedId) {
+    if(clickedId == "snickers") {
+        aVendingMachone.buySnickers()
+    } else if(clickedId == "kitkat") {
+        aVendingMachone.buyKitkat()
+    } else {
+        aVendingMachone.buyLays()
+    }
+    machineDiv.innerHTML = aVendingMachone.render()
+}
