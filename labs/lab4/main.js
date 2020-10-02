db7336a644bbb58c196882b1a8a6965f717ed469
Harman Ark <ark.harman@gmail.com>
2020-10-02 19:47:04 -0400

    added lab 4

---
+++ labs/lab4/main.js
@@ -0,0 +1,59 @@
+class TicTacToe {
+    constructor() {
+        this.gameBoardState = [[ ]]
+        this.displayBoard = showBoard()
+        this.playerTurn = 0 // 0 will be X and 1 will be 0
+        this.filledBoard = 0
+        this.displayMessage("Message goes here")
+        this.gameOver = false
+    }
+
+    showBoard() {
+        // display the board code goes here
+    }
+
+    handlePlayerTurn() {
+        if (this.gameOver || this.gameBoardState[x][y] != null) {
+            return
+        }
+
+        if(this.checkWinner()) {
+            // stop the game code goes here
+        } else if(this.filledBoard == 9) {
+            // stop the game code goes eher
+        } else {
+            // keep the game running code goes here
+        }
+    }
+
+    playerSymbol() {
+        if(this.playerTurn == 0) {
+            // make X on the board goes here
+        } else {
+            // make the O on the board goes here
+        }
+    }
+
+    switchTurn() {
+        if (this.playerTurn == 0) {
+            // X turn code goes here
+        } else {
+            // 0 turn goes here
+        }
+    }
+
+    checkWinner() {
+        // check rows code goes here
+        // check columns code goes here
+        // check 1st diagonal code goes here
+        // check 2nd diagonal code goes here
+        // otherwise return false
+    }
+
+    displayMessage(message) {
+        // if X won code goes here
+        // if 0 won code goes here
+    }
+}
+
+let aTicTacToe = new TicTacToe()
---
+++ labs/lab4/script.js
@@ -0,0 +1,86 @@
+let firstXOne = document.getElementById("firstXOne")
+let firstXTwo = document.getElementById("firstXTwo")
+let secondCircle = document.getElementById("secondCircle")
+let thirdXOne = document.getElementById("thirdXOne")
+let thirdXTwo = document.getElementById("thirdXTwo")
+let fourthCircle = document.getElementById("fourthCircle")
+let fifthCircle = document.getElementById("fifthCircle")
+let sixthXOne = document.getElementById("sixthXOne")
+let sixthXTwo = document.getElementById("sixthXTwo")
+let seventhXOne = document.getElementById("seventhXOne")
+let seventhXTwo = document.getElementById("seventhXTwo")
+let eigthCircle = document.getElementById("eigthCircle")
+let ninthXOne = document.getElementById("ninthXOne")
+let ninthXTwo = document.getElementById("ninthXTwo")
+
+
+firstXOne.addEventListener("click", onTrailClick)
+function onTrailClick(event) {
+    event.target.style.stroke = "000000"
+}
+
+firstXTwo.addEventListener("click", onTrailClick)
+function onTrailClick(event) {
+    event.target.style.stroke = "000000"
+}
+
+secondCircle.addEventListener("click", onTrailClick)
+function onTrailClick(event) {
+    event.target.style.stroke = "000000"
+}
+
+thirdXOne.addEventListener("click", onTrailClick)
+function onTrailClick(event) {
+    event.target.style.stroke = "000000"
+}
+
+thirdXTwo.addEventListener("click", onTrailClick)
+function onTrailClick(event) {
+    event.target.style.stroke = "000000"
+}
+
+fourthCircle.addEventListener("click", onTrailClick)
+function onTrailClick(event) {
+    event.target.style.stroke = "000000"
+}
+
+fifthCircle.addEventListener("click", onTrailClick)
+function onTrailClick(event) {
+    event.target.style.stroke = "000000"
+}
+
+sixthXOne.addEventListener("click", onTrailClick)
+function onTrailClick(event) {
+    event.target.style.stroke = "000000"
+}
+
+sixthXTwo.addEventListener("click", onTrailClick)
+function onTrailClick(event) {
+    event.target.style.stroke = "000000"
+}
+
+seventhXOne.addEventListener("click", onTrailClick)
+function onTrailClick(event) {
+    event.target.style.stroke = "000000"
+}
+
+seventhXTwo.addEventListener("click", onTrailClick)
+function onTrailClick(event) {
+    event.target.style.stroke = "000000"
+}
+
+eigthCircle.addEventListener("click", onTrailClick)
+function onTrailClick(event) {
+    event.target.style.stroke = "000000"
+}
+
+ninthXOne.addEventListener("click", onTrailClick)
+function onTrailClick(event) {
+    event.target.style.stroke = "000000"
+}
+
+ninthXTwo.addEventListener("click", onTrailClick)
+function onTrailClick(event) {
+    event.target.style.stroke = "000000"
+}
+
